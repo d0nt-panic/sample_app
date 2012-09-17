55d6e9a585bc30ef81a6e57e8893fce9a422857a
Yaroslav <kyzon42@gmail.com>
2012-09-17 10:34:14 +0400

    ex1ch9

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -31,7 +31,7 @@ describe "Users" do
           click_button
           response.should have_selector("div.flash.success",
                                           :content => "Welcome")
-            esponse.should render_template('users/show')
+            response.should render_template('users/show')
         end.should change(User, :count).by(1)
       end
     end
@@ -53,10 +53,7 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
